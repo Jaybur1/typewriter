e5cf583d6d32b78d637b21d301aff61f5992f680
vagrant <jayburbyga@gmail.com>
2020-03-10 15:16:01 +0000

    added typewriter

---
+++ typewriter.js
@@ -0,0 +1,15 @@
+const sentence = "hello there from lighthouse labs";
+
+const typewriter = str => {
+  let timeout = 0;
+  let fixed = str + '\n'
+  for (const char of fixed) {
+    setTimeout(() => {
+      process.stdout.write(char);
+    }, timeout);
+    timeout += 50;
+  }
+  return '\n'
+};
+
+typewriter(sentence);
